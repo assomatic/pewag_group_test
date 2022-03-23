1419c531709bde3a9a3738860d44e652d127018e
assomatic <90207584+assomatic@users.noreply.github.com>
2022-03-23 15:19:03 +0100

    icons - check color

--- data.js
+++ data.js
@@ -1,31 +1,31 @@
 const sublinks = [
   {
-    page: 'identiy',
+    page: 'identity',
     links: [
       {
         label: 'our identiy',
-        icon: 'images/menu.svg',
-        url: 'identiy.html#top',
+        icon: './images/icons/icon-fingerprint.svg',
+        url: 'identity.html#top',
       },
       {
         label: 'fakten',
-        icon: 'images/menu.svg',
-        url: 'identiy.html#fakten',
+        icon: './images/icons/icon-charts.svg',
+        url: 'identity.html#fakten',
       },
       {
         label: 'karte',
-        icon: 'images/menu.svg',
-        url: 'identiy.html#karte',
+        icon: './images/icons/icon-network.svg',
+        url: 'identity.html#karte',
       },
       {
         label: 'leitbild',
-        icon: 'images/menu.svg',
-        url: 'identiy.html#leitbild',
+        icon: './images/icons/icon-leitbild.svg',
+        url: 'identity.html#leitbild',
       },
       {
-        label: 'geschichte',
-        icon: 'images/menu.svg',
-        url: 'identiy.html#geschichte',
+        label: 'timeline',
+        icon: './images/icons/icon-timeline.svg',
+        url: 'identity.html#geschichte',
       },
     ],
   },
@@ -34,22 +34,22 @@ const sublinks = [
     links: [
       {
         label: 'our expertise',
-        icon: 'images/menu.svg',
+        icon: './images/icons/icon-expertise.svg',
         url: 'expertise.html#top',
       },
       {
         label: 'know-how',
-        icon: 'images/menu.svg',
+        icon: './images/icons/icon-know-how.svg',
         url: 'expertise.html#know-how',
       },
       {
         label: 'kompetenz',
-        icon: 'images/menu.svg',
+        icon: './images/icons/icon-kompetenz.svg',
         url: 'expertise.html#kompetenz',
       },
       {
         label: 'netzwerk',
-        icon: 'images/menu.svg',
+        icon: './images/icons/icon-network2.svg',
         url: 'expertise.html#netzwerk',
       },
     ],
@@ -59,7 +59,7 @@ const sublinks = [
     links: [
       {
         label: 'our brands',
-        icon: 'images/menu.svg',
+        icon: './images/icons/icon-brands.svg',
         url: 'brands.html#top',
       },
     ],
@@ -69,22 +69,22 @@ const sublinks = [
     links: [
       {
         label: 'our responsibility',
-        icon: 'images/menu.svg',
+        icon: './images/icons/icon-response.svg',
         url: 'responsibility.html#top',
       },
       {
         label: 'social',
-        icon: 'images/menu.svg',
+        icon: './images/icons/icon-social.svg',
         url: 'responsibility.html#social',
       },
       {
         label: 'sustainability',
-        icon: 'images/menu.svg',
+        icon: './images/icons/icon-sustainability.svg',
         url: 'responsibility.html#sustainability',
       },
       {
         label: 'goverment',
-        icon: 'images/menu.svg',
+        icon: './images/icons/icon-library.svg',
         url: 'responsibility.html#goverment',
       },
     ],
